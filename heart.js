54ae97d7feec6412c4db11bba95f0d1501a5d0fa
n-j-m <nick65@gmail.com>
2018-02-14 09:25:11 -0600

    Updates

--- heart.js
+++ heart.js
@@ -69,6 +69,16 @@
 
 "use strict";
 
+(function main() {
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.rect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'black';
+    ctx.fill();
+    ctx.strokeStyle = 'black';
+    ctx.stroke();
+})();
 
 
 /***/ })
